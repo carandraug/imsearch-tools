793efa76f8f71185cb5be7f08f0c1c57c7965ef2
David Miguel Susano Pinto <carandraug+dev@gmail.com>
2022-11-19 15:25:32 +0000

    Merge imsearchtools.http_service_helper into imsearchtools.http_service.

--- imsearchtools/__init__.py
+++ imsearchtools/__init__.py
@@ -1,11 +1,6 @@
 from gevent import monkey
 
-from . import (
-    engines as query,
-    postproc_modules,
-    process,
-    utils,
-)
+from . import engines as query, postproc_modules, process, utils
 
 
 monkey.patch_all(thread=False, select=False, httplib=False)
